4debb5d6b2502d651850a1724dc51d3f02f0e34f
unknown <nituldeori63@gmail.com>
2020-12-18 00:53:18 +0530

    Added: Ability to generate enhanced invoice

--- test/java/InvoiceServiceTest.java
+++ test/java/InvoiceServiceTest.java
@@ -19,11 +19,12 @@ public class InvoiceServiceTest {
         Assert.assertEquals(5, fare , 0.0);
     }
     @Test
-    public void givenMultipleRides_ShouldReturnTotalFare(){
+    public void givenMultipleRides_ShouldReturnInvoiceSummary(){
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         Ride[] rides = {new Ride(2.0, 5),
                         new Ride(0.1, 1)};
-        double fare = invoiceGenerator.calculateFare(rides);
-        Assert.assertEquals(30, fare, 0.0);
+        InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,30.0);
+        Assert.assertEquals(expectedInvoiceSummary, summary);
     }
 }
